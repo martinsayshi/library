07cf401e108efa0a3fdb1cc6da3261d52f459a09
Marcin Miekisz <miekiszmarcin@gmail.com>
2023-05-11 17:12:07 +0200

    Added CSS and finished JS

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-const cards = document.querySelector(".cards");
+const cards = document.querySelector(".main-cards");
 const submitBtn = document.querySelector("#submit");
 const form = document.querySelector("#form");
+const finishedBooks = document.querySelector("#finished");
+const leftBooks = document.querySelector("#left");
+const totalBooks = document.querySelector("#total");
 
-let myLibrary = [];
+let myLibrary = [
+  {
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    pages: 305,
+    read: true,
+  },
+  {
+    title: "A Game of Thrones",
+    author: "George R.R. Martin",
+    pages: 893,
+    read: false,
+  },
+  {
+    title: "The Name of the Wind",
+    author: "Patrick Rothfuss",
+    pages: 722,
+    read: true,
+  },
+  {
+    title: "American Gods",
+    author: "Neil Gaiman",
+    pages: 635,
+    read: false,
+  },
+  {
+    title: "The Princess Bride",
+    author: "William Goldman",
+    pages: 456,
+    read: false,
+  },
+];
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.id = Math.floor(Math.random() * 1000);
 }
 
 function addBookToLibrary(book) {
@@ -21,35 +56,70 @@ function addElements(element) {
   const author = document.createElement("span");
   const pages = document.createElement("span");
   const read = document.createElement("span");
+  const btnRemove = document.createElement("button");
+  const btnRead = document.createElement("button");
 
   card.classList.add("card");
+  card.setAttribute("data-id", element.id);
   title.classList.add("title");
   author.classList.add("author");
   pages.classList.add("pages");
   read.classList.add("read");
+  btnRemove.classList.add("btn", "remove");
+  btnRead.classList.add("btn", "read");
 
   title.textContent = element.title;
   author.textContent = element.author;
   pages.textContent = element.pages;
-  read.textContent = element.read;
+  read.textContent = element.read ? "Read" : "Not Read";
+  btnRemove.textContent = "Remove";
+  btnRemove.setAttribute("data-id", element.id);
+
+  btnRemove.addEventListener("click", () => {
+    const parent = btnRemove.parentElement;
+    const id = Number(btnRemove.getAttribute("data-id"));
+
+    removeBook(id);
+    parent.remove();
+  });
+  btnRead.textContent = `Mark ${element.read ? "not read" : "read"}`;
+
+  btnRead.addEventListener("click", () => {
+    element.read = !element.read;
+    read.textContent = element.read ? "Read" : "Not Read";
+    btnRead.textContent = `Mark ${element.read ? "not read" : "read"}`;
+    updateStats();
+  });
 
   card.appendChild(title);
   card.appendChild(author);
   card.appendChild(pages);
   card.appendChild(read);
+  card.appendChild(btnRemove);
+  card.appendChild(btnRead);
 
   cards.appendChild(card);
 }
 
 function displayBooks() {
   myLibrary.forEach((element) => addElements(element));
+  updateStats();
 }
 
-const book1 = new Book("HP", "JK Rowling", 345, true);
-const book2 = new Book("LOTR", "I don't know", 1545, false);
+const removeBook = (id) => {
+  myLibrary = myLibrary.filter((book) => book.id !== id);
+  updateStats();
+};
 
-addBookToLibrary(book1);
-addBookToLibrary(book2);
+updateStats = () => {
+  const finished = myLibrary.filter((book) => book.read === true).length;
+  const notFinished = myLibrary.filter((book) => book.read !== true).length;
+  const total = myLibrary.length;
+
+  finishedBooks.textContent = finished;
+  leftBooks.textContent = notFinished;
+  totalBooks.textContent = total;
+};
 
 displayBooks();
 
@@ -64,6 +134,7 @@ form.addEventListener("submit", (event) => {
   const book = new Book(title, author, pages, read);
   addBookToLibrary(book);
   addElements(book);
+  updateStats();
 
   // clear the form
   form.reset();
